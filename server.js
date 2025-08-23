75388d1d195767ec78d4e70fd746da6334290d4c
guidevi <guilhermeregis13@gmail.com>
2025-08-23 15:58:03 -0300

    consertando erro

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
